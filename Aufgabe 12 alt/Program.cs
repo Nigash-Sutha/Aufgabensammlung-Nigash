b6fbb522ab75f1b006a9069758886f8b3b63b8b2
Nigash <Nigash_Sutha@sluz.ch>
2024-11-07 15:35:36 +0100

    Update Program.cs

--- Aufgabe 12 alt/Program.cs
+++ Aufgabe 12 alt/Program.cs
@@ -10,48 +10,7 @@ namespace Aufgabe_12_alt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-------------------");
-            Console.WriteLine("Zahlen Aufsummieren");
-            Console.WriteLine("-------------------");
-            Console.WriteLine("\nGeben Sie die zu summierenden Ganzzahlen mit Komma getrennt ein: ");
-
-            string eingabe = Console.ReadLine();
-
-            string[] zahlen = eingabe.Split(',');
-
-            int[] zahlenArray = new int[zahlen.Length];
-            for (int i = 0; i < zahlen.Length; i++)
-            {
-                zahlenArray[i] = Convert.ToInt32(zahlen[i]);
-            }
-
-            int[] resultat = sumUp(zahlenArray);
-
-            Console.WriteLine("Resultat Array: " + string.Join(", ", resultat));
-
-            int summe = 0;
-            for (int i = 0; i < zahlenArray.Length; i++)
-            {
-                summe += zahlenArray[i];
-            }
-
-            Console.WriteLine("Gesamtsumme: " + summe);
-
-            Console.ReadKey();
-        }
-
-        static int[] sumUp(int[] zahlen)
-        {
-            int[] resultat = new int[zahlen.Length];
-            int summe = 0;
-
-            for (int i = 0; i < zahlen.Length; i++)
-            {
-                summe += zahlen[i];
-                resultat[i] = summe;
-            }
-
-            return resultat;
+            
         }
     }
 }
